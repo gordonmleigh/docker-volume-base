f50811ddee98b8ccc6d94e27352bd201cf19a492
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2017-02-15 17:03:39 +0000

    pass linting and move driver to new package

---
+++ driver/driver.go
@@ -0,0 +1,69 @@
+package driver
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/go-plugins-helpers/volume"
+)
+
+// BaseDriver represents a simple volume plugin driver.
+type BaseDriver struct {
+	rootPath string
+}
+
+// NewBaseDriver creates a new instance of BaseDriver.
+func NewBaseDriver(rootPath string) *BaseDriver {
+	log.WithFields(log.Fields{"rootPath": rootPath}).Info("Creating new driver BaseDriver")
+
+	d := BaseDriver{
+		rootPath: rootPath,
+	}
+	return &d
+}
+
+// Create creates a new volume.
+func (d *BaseDriver) Create(r volume.Request) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name, "Options": r.Options}).Info("REQUEST: Create volume")
+	return volume.Response{}
+}
+
+// List lists all volumes the driver knows of.
+func (d *BaseDriver) List(r volume.Request) volume.Response {
+	log.Info("REQUEST: List volumes")
+	return volume.Response{}
+}
+
+// Get gets a specific volume.
+func (d *BaseDriver) Get(r volume.Request) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name}).Info("REQUEST: Get volume")
+	return volume.Response{Volume: &volume.Volume{Name: r.Name}}
+}
+
+// Remove deletes a specific volume.
+func (d *BaseDriver) Remove(r volume.Request) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name}).Info("REQUEST: Remove volume")
+	return volume.Response{}
+}
+
+// Path gets the path of a given volume.
+func (d *BaseDriver) Path(r volume.Request) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name}).Info("REQUEST: Path")
+	return volume.Response{Err: "not found"}
+}
+
+// Mount mounts a volume onto the local file system.
+func (d *BaseDriver) Mount(r volume.MountRequest) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name, "ID": r.ID}).Info("REQUEST: Mount")
+	return volume.Response{Err: "not found"}
+}
+
+// Unmount removes a volume from the local file system.
+func (d *BaseDriver) Unmount(r volume.UnmountRequest) volume.Response {
+	log.WithFields(log.Fields{"Name": r.Name, "ID": r.ID}).Info("REQUEST: Unmount")
+	return volume.Response{Err: "not found"}
+}
+
+// Capabilities returns the capabilities of the driver
+func (d *BaseDriver) Capabilities(r volume.Request) volume.Response {
+	log.Info("REQUEST: Capabilities")
+	return volume.Response{}
+}
--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-  "os"
-  "github.com/docker/go-plugins-helpers/volume"
-  log "github.com/Sirupsen/logrus"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/go-plugins-helpers/volume"
+	"github.com/gordonmleigh/docker-volume-base/driver"
 )
 
 const (
-  driverName = "docker_volume_base"
+	driverName = "docker_volume_base"
 )
 
-
 func main() {
-  log.WithFields(log.Fields{ "Name": driverName, "Pid": os.Getpid()}).Info("*** STARTED volume driver ***")
+	log.WithFields(log.Fields{"Name": driverName, "Pid": os.Getpid()}).Info("*** STARTED volume driver ***")
 
-  d := NewBaseDriver("/tmp")
-  h := volume.NewHandler(d)
-  err := h.ServeUnix("root", driverName)
+	d := driver.NewBaseDriver("/tmp")
+	h := volume.NewHandler(d)
+	err := h.ServeTCP(driverName, ":8080", nil)
 
-  if err != nil {
-    log.Fatal(err)
-  } else {
-    log.Info("Started.")
-  }
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Info("Started.")
+	}
 }
